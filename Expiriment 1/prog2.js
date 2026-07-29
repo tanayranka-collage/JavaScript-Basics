55e6db5d29a9a95211c8f52cd1350dadcf6a81a8
tanay ranka collage <tanayranka42@gmail.com>
2026-07-29 15:08:21 +0530

    Create prog2.js

---
+++ Expiriment 1/prog2.js
@@ -0,0 +1,16 @@
+// code by tanay ranka sycse b 8
+document.title = "Program 2";
+const ans = document.getElementById("cs");
+let va = prompt("Enter a number: ");
+let f = Number(va);
+ans.style.color = "#02ff00";
+if(!isNaN(f)){
+    if(f >= 0){
+
+        ans.innerText = "Positive number";
+    }else{
+        ans.innerText = "Negative number";
+    }
+}else{
+    ans.innerText = "Give a number";
+}

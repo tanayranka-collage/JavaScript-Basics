8b1b641c3da691a99d97ef75c5add1c0a5871f9c
tanay ranka collage <tanayranka42@gmail.com>
2026-07-29 14:58:32 +0530

    Update prog1.js

--- Expiriment 1/prog1.js
+++ Expiriment 1/prog1.js
@@ -1,1 +1,14 @@
 // code by tabay ranka sycse b 8
+
+document.title = "Program 1";
+const ans = document.getElementById("cs");
+let va = Number(prompt("Enter a number: "));
+let fa = Number(prompt("Enter second Number: "));
+let f = va + fa;
+if(isNaN(f)){
+    ans.style.color = "lime";
+    ans.innerText = "Give Numerical inputt";
+}else{
+    ans.style.color = "green";
+    ans.innerText = f;
+}

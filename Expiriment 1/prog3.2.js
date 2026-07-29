dcce6415a3feb9807f0003daaf065f09fb221ac7
tanay ranka collage <tanayranka42@gmail.com>
2026-07-29 15:24:11 +0530

    Create prog3.2.js

---
+++ Expiriment 1/prog3.2.js
@@ -0,0 +1,14 @@
+// coed by tanay ranka sycse b 8
+document.title = "Program 3 - Area of Rectangle";
+const ans = document.getElementById("cs");
+let va = prompt("Enter Length: ");
+let da = prompt("Enter Breath: ");
+let f = Number(va) + Number(da);
+ans.style.color = "#02ff00";
+
+if(!isNaN(f)){
+    re = va * da;
+    ans.innerText = re.toFixed(2);
+}else{
+    ans.innerText = "Give a number";
+}
